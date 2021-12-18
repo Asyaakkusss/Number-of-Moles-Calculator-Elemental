c8509ac90977da30f7ed63eb06f0a5d8103b9d72
Asya Akkus <96194155+Asyaakkusss@users.noreply.github.com>
2021-12-17 23:31:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-num1 = input("Element Atomic Number-1:\n")
+num1 = input("Element Atomic Number:\n")
 num1 = int(num1)
 
-df = pd.read_excel(r'C:\Users\aya69\PycharmProjects\molarmasscalculator\MolarMassSpreadsheetv1.xlsx')
+num1= num1-1
+
+df = pd.read_excel(r'C:\Users\aya69\PycharmProjects\molarmasscalculator\MolarMassSpreadsheetvfinal.xlsx')
 
 num3 = input("Element mass (g):\n")
 num2 = (df['Molar Mass'].iloc[num1])
